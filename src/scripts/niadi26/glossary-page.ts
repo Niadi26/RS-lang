aa03659afb8a4df6e35ebc527eb1df9a62d030cc
Diana26 <plohayadamochka@gmail.com>
2022-02-04 23:22:50 +0300

    feat glossary css

--- src/scripts/niadi26/glossary-page.ts
+++ src/scripts/niadi26/glossary-page.ts
@@ -5,16 +5,16 @@ class GlossaryPage {
   public node: HTMLElement;
   
   constructor() {
-    const mainWrapper = new ElementHTML('div', 'wrapper', 'glosarry');
-    const buttonsCont = new ElementHTML('div', '', '', '', mainWrapper.node);
-    const buttonSprint = new ElementHTML('button', '', 'sprint', '', buttonsCont.node);
-    const buttonAudio = new ElementHTML('button', '', 'audio', '', buttonsCont.node);
-    const bookCont = new ElementHTML('div', '', '', '', mainWrapper.node);
-    const bookChapterCont = new ElementHTML('div', '', '', '', bookCont.node);
-    const bookWordsCont = new ElementHTML('div', '', '', '', bookCont.node);
-    const bookWords = new ElementHTML('div', '', '', '', bookWordsCont.node);
+    const mainWrapper = new ElementHTML('div', 'wrapper', '');
+    const buttonsCont = new ElementHTML('div', 'buttons-wrapper', '', '', mainWrapper.node);
+    const buttonSprint = new ElementHTML('button', 'game', 'sprint', '', buttonsCont.node);
+    const buttonAudio = new ElementHTML('button', 'game', 'audio', '', buttonsCont.node);
+    const bookCont = new ElementHTML('div', 'glossary__cont', '', '', mainWrapper.node);
+    const bookChapterCont = new ElementHTML('div', 'glossary__chapters', '', '', bookCont.node);
+    const bookWordsCont = new ElementHTML('div', 'glossary__pages', '', '', bookCont.node);
+    const bookWords = new ElementHTML('div', 'glossary__worlds', '', '', bookWordsCont.node);
     const bookPage = new ElementHTML('div', '', '', '', bookWordsCont.node);
-    const buttonsPageCont = new ElementHTML('div', '', '', '', bookPage.node);
+    const buttonsPageCont = new ElementHTML('div', 'buttons-wrapper', '', '', bookPage.node);
     const buttonPrev = new ElementHTML('button', '', 'prev', '', buttonsPageCont.node);
     const buttonNext = new ElementHTML('button', '', 'next', '', buttonsPageCont.node);
     this.node = mainWrapper.node;
