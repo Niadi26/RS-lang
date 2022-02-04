9ea65adebffe26a8bc747bf7708b2238c8e0ca64
Diana26 <plohayadamochka@gmail.com>
2022-02-04 22:39:43 +0300

    feat statiacics page

--- src/scripts/niadi26/navigation.ts
+++ src/scripts/niadi26/navigation.ts
@@ -1,12 +1,11 @@
 import { mainPage } from "./main-page";
 import { glosarryPage } from "./glossary-page";
-import { ElementHTML } from "./create-element";
 import { gamePage } from "./games-page";
+import { statisticsPage } from "./statistics";
 // eslint-disable-next-line prettier/prettier
 const rootElem = document.querySelector('.main') as HTMLElement;
 const navigation = document.querySelector('.nav__cont');
 
-const stat = new ElementHTML('div', '', 'stat');
 const NAVIGATION_ID = {
   main: 'main',
   glossary: 'glossary',
@@ -27,7 +26,7 @@ function createPage(id: string) {
     rootElem.append(gamePage.node);
     break;
   case NAVIGATION_ID.statistics:
-    rootElem.append(stat.node);
+    rootElem.append(statisticsPage.node);
     break;
     }
   }
---
+++ src/scripts/niadi26/statistics.ts
@@ -0,0 +1,36 @@
+import { ElementHTML } from "./create-element";
+
+class StatisticsPage {
+  // eslint-disable-next-line prettier/prettier
+  public node: HTMLElement;
+  
+  constructor() {
+    const mainWrapper = new ElementHTML('div', 'wrapper', 'glosarry');
+    const pageName = new ElementHTML('h2', '', 'Statistics for today', '', mainWrapper.node);
+    const countsCont = new ElementHTML('div', '', '', '', mainWrapper.node);
+    const newCount = new ElementHTML('div', '', '', '', countsCont.node);
+    const newNum = new ElementHTML('div', '', '0', '', newCount.node);
+    const newText = new ElementHTML('h3', '', 'New words', '', newCount.node);
+    const learnCount = new ElementHTML('div', '', '', '', countsCont.node);
+    const learnNum = new ElementHTML('div', '', '0', '', newCount.node);
+    const learnText = new ElementHTML('h3', '', 'Learned words', '', newCount.node);
+    const percentCount = new ElementHTML('div', '', '', '', countsCont.node);
+    const percentNum = new ElementHTML('div', '', '0%', '', newCount.node);
+    const percentText = new ElementHTML('h3', '', 'Right answers', '', newCount.node);
+
+    const gamesCont = new ElementHTML('div', '', '', '', mainWrapper.node);
+    const sprintCont = new ElementHTML('div', '', '', '', gamesCont.node);
+    const sprintName = new ElementHTML('h3', '', 'Sprint', '', sprintCont.node);
+    const sprintNew = new ElementHTML('h3', '', 'New words: 0', '', sprintCont.node);
+    const sprintLearn = new ElementHTML('h3', '', 'Learned words: 0', '', sprintCont.node);
+    const sprintPrecent = new ElementHTML('h3', '', 'Right answers: 0%', '', sprintCont.node);
+    const audioCont = new ElementHTML('div', '', '', '', gamesCont.node);
+    const audioName = new ElementHTML('h3', '', 'Sprint', '', audioCont.node);
+    const audioNew = new ElementHTML('h3', '', 'New words: 0', '', audioCont.node);
+    const audioLearn = new ElementHTML('h3', '', 'Learned words: 0', '', audioCont.node);
+    const audioPrecent = new ElementHTML('h3', '', 'Right answers: 0%', '', audioCont.node);
+    this.node = mainWrapper.node;
+  }
+}
+
+export const statisticsPage = new StatisticsPage();
