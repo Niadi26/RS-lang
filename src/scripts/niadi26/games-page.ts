928390ece2998e988b12d57cf3e0da1acc2cffda
Diana26 <plohayadamochka@gmail.com>
2022-02-05 23:25:36 +0300

    feat css at minigames page

--- src/scripts/niadi26/games-page.ts
+++ src/scripts/niadi26/games-page.ts
@@ -5,20 +5,30 @@ class GamePage {
     public node: HTMLElement;
     
     constructor() {
-      const mainWrapper = new ElementHTML('div', 'wrapper', 'games');
-      const gamesCont = new ElementHTML('div', '', '', '', mainWrapper.node);
+      const mainWrapper = new ElementHTML('div', 'wrapper', '');
+      const gamesCont = new ElementHTML('div', 'games__wrapper', '', '', mainWrapper.node);
 
-      const sprintCont = new ElementHTML('div', '', '', '', gamesCont.node);
-      const sprintImg = new ElementHTML('div', '', '', '', sprintCont.node);
-      const sprintName = new ElementHTML('div', '', '', '', sprintCont.node);
-      const sprintText = new ElementHTML('div', '', '', '', sprintCont.node);
-      const sprintPlay = new ElementHTML('button', '', 'play', '', sprintCont.node);
+      const sprintCont = new ElementHTML('div', 'games__cont', '', '', gamesCont.node);
+      const sprintImg = new ElementHTML('div', 'games__img ', '', '', sprintCont.node);
+      const sprintImgBg = new Image();
+      sprintImgBg.src = '../assets/niadi26/race-cat.png';
+      sprintImgBg.onload = () => {  
+        sprintImg.node.style.backgroundImage = `url('${sprintImgBg.src}')`;
+      }
+      const sprintName = new ElementHTML('div', 'games__title', 'Sprint', '', sprintCont.node);
+      const sprintText = new ElementHTML('div', 'games__text', 'Try minigame sprint!', '', sprintCont.node);
+      const sprintPlay = new ElementHTML('button', 'button-play', 'Play', '', sprintCont.node);
 
-      const audioCont = new ElementHTML('div', '', '', '', gamesCont.node);
-      const audioImg = new ElementHTML('div', '', '', '', audioCont.node);
-      const audioName = new ElementHTML('div', '', '', '', audioCont.node);
-      const audioText = new ElementHTML('div', '', '', '', audioCont.node);
-      const audioPlay = new ElementHTML('button', '', 'play', '', audioCont.node);
+      const audioCont = new ElementHTML('div', 'games__cont', '', '', gamesCont.node);
+      const audioImg = new ElementHTML('div', 'games__img ', '', '', audioCont.node);
+      const audioImgBg = new Image();
+      audioImgBg.src = '../assets/niadi26/audio-cat.png';
+      audioImgBg.onload = () => {  
+        audioImg.node.style.backgroundImage = `url('${audioImgBg.src}')`;
+      }
+      const audioName = new ElementHTML('div', 'games__title', 'Audiochallange', '', audioCont.node);
+      const audioText = new ElementHTML('div', 'games__text', 'Try minigame audio!', '', audioCont.node);
+      const audioPlay = new ElementHTML('button', 'button-play', 'Play', '', audioCont.node);
 
       this.node = mainWrapper.node;
     }
