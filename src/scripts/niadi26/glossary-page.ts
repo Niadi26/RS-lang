30c7a123ac33651b25b30636be4a3df5b6c1882e
Diana26 <plohayadamochka@gmail.com>
2022-02-05 22:11:58 +0300

    feat css at glossary page

--- src/scripts/niadi26/glossary-page.ts
+++ src/scripts/niadi26/glossary-page.ts
@@ -1,4 +1,36 @@
 import { ElementHTML } from "./create-element";
+const GROUPS_COUNT = 7; // 6 groups + difficult words
+
+const CROUPS_COLOR = [
+  'rgb(237, 20, 201)',
+  'rgb(146, 20, 237)',
+  'rgb(20, 197, 237)',
+  'rgb(99, 237, 20)',
+  'rgb(237, 230, 20)',
+  'rgb(240, 168, 60)',
+  'rgb(240, 87, 60)',
+]
+ 
+class Groups {
+  // eslint-disable-next-line prettier/prettier
+  public node: HTMLElement;
+
+  constructor(num: number) {
+    const bookGroupCont = new ElementHTML('div', 'glossary__groups', '');
+    for(let i = 0; i < num; i++) {
+      const groupCont = new ElementHTML('button', 'glossary__group', `Level  ${i + 1}`);
+      groupCont.node.setAttribute('id', `${i}`);
+      groupCont.node.style.backgroundColor = `${CROUPS_COLOR[i]}`;
+      bookGroupCont.node.append(groupCont.node);
+      if (i === num - 1) {
+        groupCont.node.innerHTML = 'Repeat';
+        groupCont.node.setAttribute('id', `X`);                            //change id
+        groupCont.node.setAttribute('disabled', 'true');
+      } 
+    }
+    this.node = bookGroupCont.node;
+  }
+}
 
 class GlossaryPage {
   // eslint-disable-next-line prettier/prettier
@@ -7,16 +39,27 @@ class GlossaryPage {
   constructor() {
     const mainWrapper = new ElementHTML('div', 'wrapper', '');
     const buttonsCont = new ElementHTML('div', 'buttons-wrapper', '', '', mainWrapper.node);
-    const buttonSprint = new ElementHTML('button', 'game', 'sprint', '', buttonsCont.node);
-    const buttonAudio = new ElementHTML('button', 'game', 'audio', '', buttonsCont.node);
+    const buttonSprint = new ElementHTML('button', 'button-glossary', '', '', buttonsCont.node);
+    const buttonSprintBg = new Image();
+    buttonSprintBg.src = '../assets/niadi26/race-cat.png';
+    buttonSprintBg.onload = () => {  
+      buttonSprint.node.style.backgroundImage = `url('${buttonSprintBg.src}')`;
+    }
+    const buttonAudio = new ElementHTML('button', 'button-glossary', '', '', buttonsCont.node);
+    const buttonAudioBg = new Image();
+    buttonAudioBg.src = '../assets/niadi26/audio-cat.png';
+    buttonAudioBg.onload = () => {  
+      buttonAudio.node.style.backgroundImage = `url('${buttonAudioBg.src}')`;
+    }
+
+    const buttonsPageCont = new ElementHTML('div', 'pagination__wrapper', '', '', mainWrapper.node);
+    const buttonPrev = new ElementHTML('button', 'button-page button-page_prev', '', '', buttonsPageCont.node);
+    const buttonNext = new ElementHTML('button', 'button-page button-page_next', '', '', buttonsPageCont.node);
+
     const bookCont = new ElementHTML('div', 'glossary__cont', '', '', mainWrapper.node);
-    const bookChapterCont = new ElementHTML('div', 'glossary__chapters', '', '', bookCont.node);
+    const bookGroupCont = new Groups(GROUPS_COUNT);
+    bookCont.node.append(bookGroupCont.node);
     const bookWordsCont = new ElementHTML('div', 'glossary__pages', '', '', bookCont.node);
-    const bookWords = new ElementHTML('div', 'glossary__worlds', '', '', bookWordsCont.node);
-    const bookPage = new ElementHTML('div', '', '', '', bookWordsCont.node);
-    const buttonsPageCont = new ElementHTML('div', 'buttons-wrapper', '', '', bookPage.node);
-    const buttonPrev = new ElementHTML('button', '', 'prev', '', buttonsPageCont.node);
-    const buttonNext = new ElementHTML('button', '', 'next', '', buttonsPageCont.node);
     this.node = mainWrapper.node;
   }
 }
