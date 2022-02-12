548e7db78d01821f64f4513e25e7b1e1e37f41a0
Diana26 <plohayadamochka@gmail.com>
2022-02-12 23:11:57 +0300

    feat: add render users words styles

--- src/scripts/components/interfaces/interface-get-word.ts
+++ src/scripts/components/interfaces/interface-get-word.ts
@@ -1,4 +1,5 @@
 export interface IWord {
+  optional: any;
   audio: string;
   audioExample: string;
   audioMeaning: string;
--- src/scripts/niadi26/render-glossary-words.ts
+++ src/scripts/niadi26/render-glossary-words.ts
@@ -1,12 +1,24 @@
 import { WordCard } from './word-card';
 import { getWords } from '../components/methods/get-words';
-import { IWord } from '../components/interfaces/get-word';
+import { getUserWords } from '../components/methods/users-words/get-user-words';
+import { IWord } from '../components/interfaces/interface-get-word';
+import { IUserWord, UserWords } from '../components/interfaces/interface-user-word';
+
+const WORDS_ON_PAGE = 20;
 
 export async function renderPage(parent?: HTMLElement) {
   const rootElement = parent || document.getElementById('words');
   const group = localStorage.getItem('glossaryGroup');
   const page = localStorage.getItem('glossaryPage');
   const autorized = localStorage.getItem('userId');
+  let userWords: UserWords;
+  if (autorized) {
+    const data = await getUserWords(autorized);
+    userWords = data.filter(
+      (el: IUserWord) =>
+        el.optional.page === page && el.optional.group === group && (el.optional.difficult || el.optional.learned)
+    );
+  }
   const data = await getWords(group, page);
   if (rootElement) {
     rootElement.innerHTML = '';
@@ -27,6 +39,14 @@ export async function renderPage(parent?: HTMLElement) {
         el.audioExample,
         autorized
       );
+      if (userWords.find((word: IUserWord) => word.wordId === item.node.id && word.optional.learned)) {
+        item.flag.classList.add('dificult-easy');
+      } else if (userWords.find((word: IUserWord) => word.wordId === item.node.id && word.optional.difficult)) {
+        item.flag.classList.add('dificult-hard');
+      }
+      if (userWords.length === WORDS_ON_PAGE) {
+        item.node.classList.add('glossary__pages-learned');
+      }
       rootElement.append(item.node);
     });
   }
