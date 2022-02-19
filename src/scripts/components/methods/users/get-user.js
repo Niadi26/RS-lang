d5c492146b304216692673685eed5b888c49ba9a
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-02-20 00:44:37 +0300

    feat: add refreshtoken

--- src/scripts/components/methods/users/get-user.js
+++ src/scripts/components/methods/users/get-user.js
@@ -1,23 +1,38 @@
 import { callApi } from '../call-api';
+import { getUserTokens } from './get-user-tokens';
 
 export const getUser = async (integer) => {
   const token = localStorage.getItem('token');
+  const method = 'GET';
+  const url = `/users/${integer}`;
+  const body = {};
   try {
-    const method = 'GET';
-    const url = `/users/${integer}`;
     const headers = {
       Accept: 'application/json',
       'Content-Type': 'application/json',
       Authorization: `Bearer ${JSON.parse(token)}`,
     };
-    const body = {};
     const response = await callApi(method, url, body, headers);
-    const dataUser = response.data;
     // console.log(response.data);
-    return dataUser;
+    return response.data;
   } catch (error) {
     console.log(error);
+    if (error.response.status === 401) {
+      const dataRes = await getUserTokens(integer);
+      const newToken = JSON.stringify(dataRes.token);
+      const newRefreshToken = JSON.stringify(dataRes.refreshToken);
+      localStorage.setItem('token', newToken);
+      localStorage.setItem('refreshToken', newRefreshToken);
+    }
+    token = localStorage.getItem('token');
+    const headers = {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${JSON.parse(token)}`,
+    };
+    const response = await callApi(method, url, body, headers);
+    return response.data;
   }
 };
 
-// getUser('61fe572351aa2800163c35a3');
+// getUser('621126d94806ad0016df10dc');
