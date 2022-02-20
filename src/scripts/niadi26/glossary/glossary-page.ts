9abdd9608edd51d581353e01efdf45547e50ecd4
Diana26 <plohayadamochka@gmail.com>
2022-02-20 22:41:41 +0300

    fix: glossary page conflict

--- src/scripts/niadi26/glossary/glossary-page.ts
+++ src/scripts/niadi26/glossary/glossary-page.ts
@@ -4,8 +4,18 @@ import { renderPage } from "./render-glossary-words";
 import { startGame } from "../../mowgle88/sprint/sprint-page";
 import { sprintGameContent } from "../../mowgle88/sprint/sprint-page-contant";
 import { rootElem } from "../../components/constants";
-import { MAX_PAGE, GROUPS_COUNT, GROUPS_COLOR } from './constants';
 
+const GROUPS_COUNT = 7; // 6 groups + difficult words
+export const MAX_PAGE = 29;
+ export const GROUPS_COLOR = [
+  'rgb(237, 20, 201)',
+  'rgb(146, 20, 237)',
+  'rgb(20, 197, 237)',
+  'rgb(99, 237, 20)',
+  'rgb(237, 230, 20)',
+  'rgb(240, 168, 60)',
+  'rgb(240, 87, 60)',
+]
 class Groups {
   // eslint-disable-next-line prettier/prettier
   public node: HTMLElement;
